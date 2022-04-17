796b2398e76a312e8aad509c0f885ea91f7f74d0
malfell <malfellows13@gmail.com>
2022-04-16 23:46:49 -0500

    refactored code to move any object

--- index.js
+++ index.js
@@ -2,60 +2,10 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null; //direction has to be null? So it can change?
-let x = 100; //current position for Link
-let y = 250;
+move(character).withArrowKeys(100, 250)
 
-// Function to move the character
-setInterval(function(){
-    if(direction === 'west'){
-        x = x -1
-    }
-
-    if(direction === 'north'){
-        y = y + 1
-    }
-
-    if(direction === 'east'){
-        x = x + 1
-    }
-
-    if(direction === 'south'){
-        y = y -1
-    }
-
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1) //calls moveCharacter function every millisecond
-
-// When no keys are pressed, the character
-//stays still
-document.addEventListener('keyup', function(e){
-    direction = null
-})
-
-// Click arrow keys for movement! "e" needed for checking
-//which key was pressed. When keys are pressed, character moves.
-document.addEventListener('keydown', function(e){
-    //let user hold down key to keep movement happening
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-
-move(character).to(100, 250)
+// Test to move tree around
+// move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 
 
 move(newImage('assets/tree.png')).to(200, 450)
--- move.js
+++ move.js
@@ -6,7 +6,68 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom){
+        let direction = null; //direction has to be null? So it can change?
+        let x = left; //current position for thing
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        // Function to move the element
+        function moveCharacter(){
+            if(direction === 'west'){
+                x-=1
+            }
+
+            if(direction === 'north'){
+                y+=1
+            }
+
+            if(direction === 'east'){
+                x+=1
+            }
+
+            if(direction === 'south'){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        //calls move function every millisecond
+        setInterval(moveCharacter, 1)
+
+        // Click arrow keys for movement! "e" needed for checking
+        //which key was pressed. When keys are pressed, element moves.
+        document.addEventListener('keydown', function(e){
+            //let user hold down key to keep movement happening
+            if(e.repeat) return;
+
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+
+        })
+
+        // When no keys are pressed, the thing
+        //stays still
+        document.addEventListener('keyup', function(e){
+            direction = null
+
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
